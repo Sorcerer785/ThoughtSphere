170d1bab67ef1d6df073029511788cd8a10193bd
Sorcerer785 <saurabhnarota1404@gmail.com>
2025-12-18 13:06:43 +0530

    bug fix

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -45,6 +45,7 @@ router.post('/register', async (req, res) => {
     res.cookie('refreshToken', refreshToken, {
       httpOnly: true,
       secure: true, //process.env.NODE_ENV === 'production',
+      secure: true, //process.env.NODE_ENV === 'production',
       sameSite: 'none',
       maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
     });
@@ -91,8 +92,8 @@ router.post('/login', async (req, res) => {
     // Set httpOnly cookie for refresh token
     res.cookie('refreshToken', refreshToken, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
-      sameSite: 'strict',
+      secure: true, //process.env.NODE_ENV === 'production',
+      sameSite: 'none',
       maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
     });
 
